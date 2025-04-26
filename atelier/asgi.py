15f7b3e5a266bdedee7430e9b141f410603687c8
worthyapple <kundwanikhushi6@gmail.com>
2025-04-26 20:18:18 +0530

    Add files via upload

--- atelier/asgi.py
+++ atelier/asgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'atelier.settings')
 
 application = get_asgi_application()
+
+# --- ADD THIS LINE ---
+app = application
