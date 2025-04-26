171531ac2a1dd46e5f4947103dd957a2eb93a309
worthyapple <kundwanikhushi6@gmail.com>
2025-04-26 20:10:44 +0530

    Delete atelier directory

--- atelier/web/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import ContactFormSubmission, IndexFormSubmission
-
-admin.site.register(ContactFormSubmission)
-admin.site.register(IndexFormSubmission)
