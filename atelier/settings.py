628d2a6add240c425f40042af365b5e3e717de0c
worthyapple <kundwanikhushi6@gmail.com>
2025-04-27 11:41:40 +0530

    Update settings.py

--- atelier/settings.py
+++ atelier/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!x21hl12u9i$@ujuymn1u+v04=$ig&16@y!5gjxl&=9q0l%@d3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
